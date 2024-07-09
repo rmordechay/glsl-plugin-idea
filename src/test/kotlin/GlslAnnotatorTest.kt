87a765711ef68f14a08aa10ef9b89366eb4f92fa
walt-grace <roi3363@yahoo.com>
2024-07-09 04:46:30 +0200

    Wrap include lexer method

--- src/test/kotlin/GlslAnnotatorTest.kt
+++ src/test/kotlin/GlslAnnotatorTest.kt
@@ -17,6 +17,7 @@ class GlslAnnotatorTest : BasePlatformTestCase() {
         myFixture.checkHighlighting(false, true, false)
     }
 
+    @Test
     fun testAnnotatorFile3() {
         myFixture.configureByFile("AnnotatorFile3.glsl")
         myFixture.checkHighlighting(false, false, false)

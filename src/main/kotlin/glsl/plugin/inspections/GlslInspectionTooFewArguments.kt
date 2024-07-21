3f4b0326c2f6fc204e4b9688a059cde0b5debea3
walt-grace <roi3363@yahoo.com>
2024-07-21 19:56:40 +0200

    Moved annotator test to inspections

--- src/main/kotlin/glsl/plugin/inspections/GlslInspectionTooFewArguments.kt
+++ src/main/kotlin/glsl/plugin/inspections/GlslInspectionTooFewArguments.kt
@@ -20,21 +20,18 @@ class GlslInspectionTooFewArguments : LocalInspectionTool() {
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
         return object: GlslVisitor() {
             override fun visitFunctionCall(functionCall: GlslFunctionCall) {
-                val constructorIdentifier = functionCall.typeSpecifier?.typeName
-                if (constructorIdentifier != null) {
-                    val structSpecifier = constructorIdentifier.reference?.resolve() as? GlslStructSpecifier ?: return
-                    val expectedParamCount = structSpecifier.getStructMembers().size
-                    val actualParamsExprs = functionCall.exprNoAssignmentList
-                    if (expectedParamCount > actualParamsExprs.size) {
-                        val msg = GlslErrorMessages.TOO_FEW_ARGUMENTS_CONSTRUCTOR.format(constructorIdentifier.getName())
-                        val startOffset = functionCall.leftParen.textRange.startOffset
-                        val endOffset = functionCall.rightParen.textRange.endOffset
-                        holder.registerProblem(functionCall, msg,
-                            ProblemHighlightType.GENERIC_ERROR,
-                            TextRange(startOffset, endOffset)
-                        )
-                    }
-                }
+                val constructorIdentifier = functionCall.typeSpecifier?.typeName ?: return
+                val structSpecifier = constructorIdentifier.reference?.resolve() as? GlslStructSpecifier ?: return
+                val expectedParamCount = structSpecifier.getStructMembers().size
+                val actualParamsExprs = functionCall.exprNoAssignmentList
+                if (expectedParamCount <= actualParamsExprs.size) return
+                val msg = GlslErrorMessages.TOO_FEW_ARGUMENTS_CONSTRUCTOR.format(constructorIdentifier.getName())
+                val startOffset = functionCall.leftParen.textRange.startOffset
+                val endOffset = functionCall.rightParen.textRange.endOffset
+                holder.registerProblem(functionCall, msg,
+                    ProblemHighlightType.GENERIC_ERROR,
+                    TextRange(startOffset, endOffset)
+                )
             }
         }
     }
--- src/test/kotlin/GlslInspectionsTest.kt
+++ src/test/kotlin/GlslInspectionsTest.kt
@@ -1,11 +1,51 @@
-import com.intellij.testFramework.UsefulTestCase
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import glsl.plugin.inspections.*
 
-class GlslInspectionsTest : UsefulTestCase() {
-    override fun getHomePath(): String {
-        return super.getHomePath()
+class GlslInspectionsTest : BasePlatformTestCase() {
+
+    override fun setUp() {
+        super.setUp()
+        myFixture.enableInspections(
+            GlslInspectionTooFewArguments(),
+            GlslInspectionTooManyArguments(),
+            GlslInspectionIncompatibleType(),
+            GlslInspectionMissingReturn(),
+            GlslInspectionNoMatchingFunction(),
+            GlslInspectionOperatorDoesNotOperate(),
+        )
     }
 
-    fun testInspection() {
+    override fun getTestDataPath(): String {
+        return "src/test/testData/inspections"
+    }
 
+    fun testAnnotator1() {
+        myFixture.configureByFile("InspectionsFile1.glsl")
+        myFixture.checkHighlighting(false, false, false)
+    }
+
+    fun testAnnotator2() {
+        myFixture.configureByFile("InspectionsFile2.frag")
+        myFixture.checkHighlighting(false, false, false)
+    }
+
+    fun testAnnotator3() {
+        myFixture.configureByFiles("InspectionsFile3.glsl")
+        myFixture.checkHighlighting(false, false, false)
+    }
+
+    fun testAnnotator4() {
+        myFixture.configureByFiles("InspectionsFile4.glsl")
+        myFixture.checkHighlighting(false, false, false)
+    }
+
+    fun testAnnotator5() {
+        myFixture.configureByFiles("InspectionsFile5.glsl")
+        myFixture.checkHighlighting(false, false, false)
+    }
+
+    fun testAnnotator6() {
+        myFixture.configureByFiles("InspectionsFile6.glsl")
+        myFixture.checkHighlighting(false, false, false)
     }
 }

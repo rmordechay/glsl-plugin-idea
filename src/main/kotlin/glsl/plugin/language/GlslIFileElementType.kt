785d40f115e7a7ef04a6c80a06ebcae642d08ee8
FalsePattern <me@falsepattern.com>
2025-01-14 02:26:16 +0100

    recursive includes for lexer type resolution

--- src/main/kotlin/glsl/plugin/language/GlslIFileElementType.kt
+++ src/main/kotlin/glsl/plugin/language/GlslIFileElementType.kt
@@ -2,6 +2,7 @@ package glsl.plugin.language
 
 import com.intellij.lang.ASTNode
 import com.intellij.lang.impl.PsiBuilderImpl
+import com.intellij.openapi.vfs.originalFileOrSelf
 import com.intellij.psi.PsiElement
 import com.intellij.psi.tree.IFileElementType
 
@@ -12,7 +13,7 @@ class GlslIFileElementType : IFileElementType(GlslLanguage.INSTANCE) {
     override fun doParseContents(chameleon: ASTNode, psi: PsiElement): ASTNode? {
         val project = psi.project
         val parserDefinition = GlslParserDefinition()
-        val builder = PsiBuilderImpl(project, parserDefinition, GlslLexer(project, psi.containingFile.virtualFile), chameleon, chameleon.chars)
+        val builder = PsiBuilderImpl(project, parserDefinition, GlslLexer(project, psi.containingFile.viewProvider.virtualFile.originalFileOrSelf()), chameleon, chameleon.chars)
         val parser = GlslParserAdapter()
         val rootNode = parser.parse(this, builder)
         return rootNode.firstChildNode
